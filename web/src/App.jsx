f3ebe09fb1ad3ed9ca8ce2471ac69eb39ccf0ce9
Daniel Fain <d6niel@protonmail.com>
2019-06-05 05:27:40 -0400

    auto-resizing of high res images into cards

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -3,11 +3,10 @@ import React from 'react';
 import ImageCard from './ImageCard';
 
 const App = () => (
-  <div>
+  <div style={{ paddingLeft: '7vw', paddingRight: '7vw', paddingTop: '2vw', paddingBottom: '2vw' }}>
     <h1>Imgur Clone</h1>
-    <div style={{ paddingLeft: '5vw' }}>
-      <ImageCard header="Raul the Dog" labels={['Dog', 'Cute']} uploadDate="April 8th, 2019" url="https://i.imgur.com/imgxeSd_d.jpg?maxwidth=520&amp;shape=thumb&amp;fidelity=high" />
-    </div>
+    <ImageCard header="Raul the Dog" labels={['Dog', 'Cute']} uploadDate="April 8th, 2019" url="https://i.imgur.com/DBbtjie.jpg" />
+    <ImageCard header="Painting" labels={['Mad', 'Kids']} uploadDate="April 25th, 2019" url="https://i.imgur.com/dVHbFf9.jpg" />
   </div>
 );
 
