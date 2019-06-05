bd817caeeb82e49198d7915b67d92e828630626f
Daniel Fain <d6niel@protonmail.com>
2019-06-04 21:46:56 -0400

    tweak dummy info

--- web/src/App.js
+++ web/src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div>
         <h1>Imgur Clone</h1>
         <div style={{ paddingLeft: '5vw' }}>
-          <ImageCard header='My Nigga' labels={['Dog', 'Cute']} uploadDate='April 8th, 2019' url='https://i.imgur.com/imgxeSd_d.jpg?maxwidth=520&amp;shape=thumb&amp;fidelity=high' />
+          <ImageCard header='Raul the Dog' labels={['Dog', 'Cute']} uploadDate='April 8th, 2019' url='https://i.imgur.com/imgxeSd_d.jpg?maxwidth=520&amp;shape=thumb&amp;fidelity=high' />
         </div>
       </div>
     );
