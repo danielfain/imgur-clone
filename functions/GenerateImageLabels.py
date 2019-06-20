2a0bbdd5fb82cca8f38c6e85ebb3f90ce3cc8f59
Daniel Fain <d6niel@protonmail.com>
2019-06-19 23:39:00 -0400

    configure s3 trigger, dynamodb, and labels

--- functions/GenerateImageLabels.py
+++ functions/GenerateImageLabels.py
@@ -3,20 +3,45 @@ import uuid
 import json
 
 rekognition = boto3.client("rekognition", "us-east-1")
+dynamo = boto3.resource("dynamodb", "us-east-1")
 
 max_labels = 3
 min_confidence = 80
 
+
 def lambda_handler(event, context):
+    image_key = event["Records"][0]["s3"]["object"]["key"]
+    
     response = rekognition.detect_labels(
         Image = {
             "S3Object": {
-                "Bucket": "danielfain",
-                "Name": event["key"],
+                "Bucket": "imgurclone",
+                "Name": image_key,
             }
         },
         MaxLabels = max_labels,
         MinConfidence = min_confidence
-    ) 
+    )
+
+    labels = []
     
-    return response["Labels"]
+    for label in response["Labels"]:
+        labels.append(label["Name"])
+    
+    key = uuid.uuid4().hex
+    table = dynamo.Table("imgur_clone_images")
+
+    table.put_item(
+        Item = {
+            "key": key,
+            "image_key": image_key,
+            "labels": labels
+        }
+    )
+
+    response = {
+        "statusCode": 200,
+        "body": "success"
+    }
+
+    return response
