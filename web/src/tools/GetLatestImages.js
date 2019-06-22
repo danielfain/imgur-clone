ee9d626b7eb1fea87868c456dd63bd30cf7f6234
Daniel Fain <d6niel@protonmail.com>
2019-06-21 22:17:10 -0400

    react environment variables tweak

--- web/src/tools/GetLatestImages.js
+++ web/src/tools/GetLatestImages.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const MAX_IMAGES = 20;
-const API_URL = `${process.env.GET_LATEST_IMAGES_API}?limit=${MAX_IMAGES}`;
+const API_URL = `${process.env.REACT_APP_GET_LATEST_IMAGES_API}?limit=${MAX_IMAGES}`;
 
 axios.get(API_URL)
   .then((images) => {
